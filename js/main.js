95d231b8944ebd3bf60f378dc62bdb40d1bd2edb
= <herniaan@gmail.com>
2016-09-03 15:10:23 +0900

    Add additional menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function(){
-  $("body").html(
-    $("body").html().replace("を","お")
-  );
+  // メニュー追加
+  $("ul.navmenu").append('<li class="item shop_menu_map_display_toggle"><a href="#">池田メニュー</a></li>');
 });
